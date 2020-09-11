ac1dc3a57da1785c33dea8d085ce5f517ca0e73a
Codingbug <240615534@qq.com>
2020-09-11 17:06:22 +0800

    Create condition.h

---
+++ condition.h
@@ -0,0 +1,40 @@
+#pragma once
+#include"mutex.h"
+#include<boost/noncopyable.hpp>
+
+namespace mudo {
+	class Condition :boost::noncopyable {
+	public:
+		explicit Condition(MutexLock& mutex) :mutex_(mutex) {
+			pthread_cond_init(&pcond_, NULL);
+		}
+
+		~Condition() {
+			pthread_cond_destroy(&pcond_);
+		}
+
+		void wait() {
+			pthread_cond_wait(&pcond_,mutex_.getPthreadMutex());
+		}
+
+		bool waitForSeconds(int seconds) {
+			struct timespec abstime;
+			clock_gettime(CLOCK_REALTIME, &abstime);
+			abstime.tv_nsec += seconds;
+			return ETIMEDOUT == pthread_cond_timedwait(&pcond_, mutex_.getPthreadMutex(), &abstime);
+		}
+
+		void notify() {
+			pthread_cond_signal(&pcond_);
+		}
+
+		void notifyAll() {
+			pthread_cond_broadcast(&pcond_);
+		}
+
+	private:
+		MutexLock& mutex_;
+		pthread_cond_t pcond_;
+	};
+
+}
